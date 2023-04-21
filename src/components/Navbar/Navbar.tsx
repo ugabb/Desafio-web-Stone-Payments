2ae39114cad8cdce6b871a311e4789fd3a292cd4
ugabb <gsb.alternativo@gmail.com>
2023-04-21 10:37:11 -0300

    MyDateTimeFormateOptions interface added on navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,11 +5,35 @@ import StoneLogo from "../StoneLogo";
 
 type Props = {};
 
+interface MyDateTimeFormatOptions extends Intl.DateTimeFormatOptions {
+
+  // * Formata o ano como "numeric" ou "2-digit".
+  year?: 'numeric' | '2-digit';
+  
+  // * Formata o mês como "numeric", "2-digit", "narrow", "short" ou "long".
+  month?: 'numeric' | '2-digit' | 'narrow' | 'short' | 'long';
+  
+  // * Formata o dia como "numeric" ou "2-digit".
+  day?: 'numeric' | '2-digit';
+
+  // * Formata a hora como "numeric" ou "2-digit".
+  hour?: 'numeric' | '2-digit';
+  
+  // * Formata os minutos como "numeric" ou "2-digit".
+  minute?: 'numeric' | '2-digit';
+  
+  // * Formata os segundos como "numeric" ou "2-digit".
+  second?: 'numeric' | '2-digit';
+  
+  // * Formata o nome do fuso horário como "short" ou "long".
+  timeZoneName?: 'short' | 'long';
+}
+
 
 const Navbar = (props: Props) => {
   // Pegando dados atuais de data e horario
   const today = new Date();
-  const options = { year: "numeric", month: "long", day: "numeric" };
+  const options: MyDateTimeFormatOptions = { year: "numeric", month: "long", day: "numeric" };
   const formatDate = today.toLocaleDateString("pt-BR", options).replace(' de ', ' ');
   const hour = today.getHours() 
   const minutes = today.getMinutes() 
